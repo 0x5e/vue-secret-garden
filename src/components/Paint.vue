a809eba5680eb5f33f7789970671128126ff2aa1
gaosen <0x5e@sina.cn>
2017-05-04 17:56:22 +0800

    hammerjs

--- src/components/Paint.vue
+++ src/components/Paint.vue
@@ -1,34 +1,37 @@
 <template>
   <div class="paint">
-    <canvas id="canvas" width="100vw" height="100vh" v-on:click="click" />
+    <canvas id="canvas" v-bind:style="canvasStyle" v-on:click="click" />
     <div class="tool-bar">
       <div id="back-btn" v-on:click="backward" />
       <div id="done-btn" v-on:click="next" />
       <div id="forward-btn" v-on:click="forward" />
     </div>
     <div class="picker-container" v-if="!pickerHidden" >
-      <div id="unfold-btn" v-on:click="fold" />
+      <div id="unfold-btn" v-on:click="pickerHidden = !pickerHidden" />
       <color-picker ref="picker" v-bind:colors="$route.params.colors" selection="true" />
     </div>
     <div class="picker-container" v-else >
-      <div id="fold-btn" v-on:click="fold" />
+      <div id="fold-btn" v-on:click="pickerHidden = !pickerHidden" />
     </div>
   </div>
 </template>
 
 <script>
-import 'floodfill'
-import PinchZoom from 'pinch-zoom'
 import ColorPicker from './ColorPicker'
-import $ from 'jquery' // eslint-disable-line
-import Panzoom from 'jquery.panzoom' // eslint-disable-line
+import sprintf from 'sprintf' // eslint-disable-line
+import floodfill from 'floodfill' // eslint-disable-line
+import Hammer from 'hammerjs' // eslint-disable-line
 
 export default {
   name: 'paint',
   data () {
     return {
       pickerHidden: false,
-      pinchZoom: null
+      offset: {x: 0, y: 0},
+      translate: {x: 0, y: 0},
+      scale: 1,
+      minScale: 1,
+      maxScale: 1
     }
   },
 
@@ -38,6 +41,13 @@ export default {
     },
     ctx () {
       return this.canvas.getContext('2d')
+    },
+    canvasStyle () {
+      return {
+        marginLeft: this.offset.x + 'px',
+        marginTop: this.offset.y + 'px',
+        transform: sprintf('translate(%dpx, %dpx) scale(%f, %f)', this.translate.x, this.translate.y, this.scale, this.scale)
+      }
     }
   },
 
@@ -59,49 +69,85 @@ export default {
   methods: {
     loadImage () {
       let img = new Image()
-      img.src = this.$route.params.img
-
-      this.canvas.hidden = true
       img.onload = () => {
+        console.log('canvas loaded')
         this.canvas.hidden = false
+
+        // 画布大小
         this.canvas.width = img.width
         this.canvas.height = img.height
 
+        // 画布居中
+        this.offset = {x: (window.innerWidth - this.canvas.width) / 2, y: (window.innerHeight - this.canvas.height) / 2}
+
+        // 缩放范围
+        let scaleX = window.innerWidth / this.canvas.width
+        let scaleY = window.innerHeight / this.canvas.height
+        console.log('scale', scaleX, scaleY)
+
+        let minScale = Math.min(scaleX, scaleY)
+        let maxScale = Math.max(scaleX, scaleY)
+
+        this.minScale = minScale
+        this.maxScale = maxScale * 4.0
+        // this.scale = maxScale
+
         this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height)
 
-        // todo 性能优化
-        this.pinchZoomInit()
-
-        // this.PanzoomInit()
+        this.HammerInit()
       }
+      this.canvas.hidden = true
+      img.src = this.$route.params.img
     },
 
-    pinchZoomInit () {
-      this.pinchZoom = PinchZoom(this.canvas, {
-        draggable: true,
-        maxScale: 5
-      })
-    },
+    HammerInit () {
+      var offset = {}
+      var scale = 1.0
 
-    PanzoomInit () {
-      $('#canvas').panzoom()
-      $('#canvas').panzoom('option', {
-        increment: 1,
-        minScale: 0.2,
-        maxScale: 5,
-        duration: 500
+      let mc = new Hammer(this.canvas)
+
+      // 画布偏移量
+      mc.get('pan').set({direction: Hammer.DIRECTION_ALL})
+      mc.on('panstart', (ev) => {
+        offset = {x: this.translate.x, y: this.translate.y}
+        console.log('panstart', offset)
       })
-      $('#canvas').on('mousedown touchstart', (e) => {
-        e.stopImmediatePropagation()
+      mc.on('panmove', (ev) => {
+        this.translate = {x: offset.x + ev.deltaX, y: offset.y + ev.deltaY}
       })
-      // $('#canvas').on('touchstart', this.click)
-      // $('#canvas').click(this.click)
+
+      // 画布缩放
+      mc.get('pinch').set({enable: true})
+      mc.on('pinchstart', (ev) => {
+        scale = this.scale
+        console.log('pinchstart', scale)
+      })
+      mc.on('pinch', (ev) => {
+        this.scale = scale * ev.scale
+        this.scale = Math.min(this.scale, this.maxScale)
+        this.scale = Math.max(this.minScale, this.scale)
+      })
+
+      // mc.on('tap', (ev) => {
+      //   console.log(ev)
+      //   let point = {x: ev.center.x - this.translate.x, y: ev.center.y - this.translate.y}
+      //   console.log('tap', point)
+
+      //   let data = this.ctx.getImageData(point.x, point.y, 1, 1).data
+      //   if (data[0] === 0 && data[1] === 0 && data[2] === 0 && data[3] === 255) {
+      //     console.log('skip the line')
+      //     return
+      //   }
+
+      //   this.ctx.fillStyle = this.$refs.picker.color
+      //   this.ctx.fillFlood(point.x, point.y, 32)
+      // })
     },
 
     click (event) {
       let x = event.offsetX
       let y = event.offsetY
-      console.log('click: ' + [x, y])
+      console.log('click', {x: x, y: y})
 
       let data = this.ctx.getImageData(x, y, 1, 1).data
       if (data[0] === 0 && data[1] === 0 && data[2] === 0 && data[3] === 255) {
@@ -113,12 +159,8 @@ export default {
       this.ctx.fillFlood(x, y, 32)
     },
 
-    fold () {
-      this.pickerHidden = !this.pickerHidden
-    },
-
     backward () {
-      this.pinchZoom.reset()
+      this.offset = {x: 0, y: 0}
     },
 
     forward () {
@@ -147,7 +189,11 @@ export default {
 .paint {
   width: 100vw;
   height: 100vh;
-  overflow: scroll;
+  overflow: overlay;
+  background-color: #f0f0f0;
+}
+#canvas {
+  background-color: white;
 }
 .picker-container {
   position: fixed;
@@ -173,9 +219,6 @@ export default {
   width: 5vw;
   height: 10vw;
   background: url(../assets/fold.svg) no-repeat center;
-}
-#canvas {
-  background: #ffffff;
 }
 .tool-bar {
   position: fixed;
